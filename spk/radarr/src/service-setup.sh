afed028d35933f306a4af27b50d45db717b1b885
ta264 <ta264@users.noreply.github.com>
2022-05-04 21:59:57 +0100

    Don't use bwrap for DSM 7

--- spk/radarr/src/service-setup.sh
+++ spk/radarr/src/service-setup.sh
@@ -1,25 +1,34 @@
-
-# Radarr service setup
-
-BWRAP="${SYNOPKG_PKGDEST}/bin/bwrap"
-RADARR="/usr/lib/radarr/bin/Radarr"
-
-# Radarr uses custom Config and PID directories
-PID_FILE="${SYNOPKG_PKGVAR}/radarr.pid"
+APP_LOWER="radarr"
+APP_UPPER="Radarr"
 
 GROUP="sc-download"
 
-# CONFIG_DIR is the directory inside the chroot
-CONFIG_DIR="/var/lib/radarr"
+USR_LIB="/usr/lib/${APP_LOWER}"
+PID_FILE="${SYNOPKG_PKGVAR}/${APP_LOWER}.pid"
 
-SERVICE_COMMAND="${BWRAP} --bind ${SYNOPKG_PKGDEST}/rootfs / --proc /proc --dev /dev --bind ${SYNOPKG_PKGVAR} ${CONFIG_DIR} --bind /volume1 /volume1 --setenv HOME ${SYNOPKG_PKGVAR} ${RADARR} --nobrowser --data=${CONFIG_DIR}"
+# Some versions include bwrap to create a chroot container with newer libraries.
+# If the bwrap binary is in the package, use the chroot
+BWRAP="${SYNOPKG_PKGDEST}/bin/bwrap"
+
+if [ -f "${BWRAP}" ]; then
+    APP="${USR_LIB}/bin/${APP_UPPER}"
+    CONFIG_DIR="/var/lib/${APP_LOWER}"
+    SERVICE_COMMAND="${BWRAP} --bind ${SYNOPKG_PKGDEST}/rootfs / --proc /proc --dev /dev --bind ${SYNOPKG_PKGDEST}${USR_LIB} ${USR_LIB} --bind ${SYNOPKG_PKGVAR} ${CONFIG_DIR} --bind /volume1 /volume1 --setenv HOME ${SYNOPKG_PKGVAR} ${APP} --nobrowser --data=${CONFIG_DIR}"
+else
+    APP="${SYNOPKG_PKGDEST}${USR_LIB}/bin/${APP_UPPER}"
+    SERVICE_COMMAND="env HOME=${SYNOPKG_PKGVAR} ${APP} --nobrowser --data=${SYNOPKG_PKGVAR}"
+fi
+
 SVC_BACKGROUND=y
 
 fix_permissions ()
 {
     if [ ${SYNOPKG_DSM_VERSION_MAJOR} -lt 7 ]; then
-        chown root:root "${BWRAP}"
-        chmod u+s "${BWRAP}"
+        if [ -f "${BWRAP}" ]; then
+            chown root:root "${BWRAP}"
+            chmod u+s "${BWRAP}"
+        fi
+
         set_unix_permissions "${SYNOPKG_PKGVAR}"
     fi
 }
@@ -36,7 +45,7 @@ service_postupgrade ()
 {
     # Make Radarr do an update check on start to avoid possible Radarr
     # downgrade when synocommunity package is updated
-    touch ${CONFIG_DIR}/update_required
+    touch ${SYNOPKG_PKGVAR}/update_required
 
     fix_permissions
 }
