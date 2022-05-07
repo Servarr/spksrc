966ba6488263463a9d16a090aa310e12dfa95d70
ta264 <ta264@users.noreply.github.com>
2022-05-07 21:13:35 +0100

    Add prowlarr

---
+++ spk/prowlarr/src/service-setup.sh
@@ -0,0 +1,55 @@
+APP_LOWER="prowlarr"
+APP_UPPER="Prowlarr"
+
+GROUP="sc-download"
+
+USR_LIB="/usr/lib/${APP_LOWER}"
+PID_FILE="${SYNOPKG_PKGVAR}/${APP_LOWER}.pid"
+
+# If the bwrap binary is in this package, use it, otherwise use from standalone package.
+BWRAP="${SYNOPKG_PKGDEST}/bin/bwrap"
+if [ ! -f "${BWRAP}" ]; then
+    BWRAP="/var/packages/bubblewrap/target/bin/bwrap"
+fi
+
+# Some versions include a rootfs to create a chroot container with newer libraries.
+ROOTFS="${SYNOPKG_PKGDEST}/rootfs"
+if [ -d "${ROOTFS}" ]; then
+    APP="${USR_LIB}/bin/${APP_UPPER}"
+    CONFIG_DIR="/var/lib/${APP_LOWER}"
+    SERVICE_COMMAND="${BWRAP} --bind ${ROOTFS} / --proc /proc --dev /dev --bind ${SYNOPKG_PKGDEST}${USR_LIB} ${USR_LIB} --bind ${SYNOPKG_PKGVAR} ${CONFIG_DIR} --bind /volume1 /volume1 --setenv HOME ${SYNOPKG_PKGVAR} ${APP} --nobrowser --data=${CONFIG_DIR}"
+else
+    APP="${SYNOPKG_PKGDEST}${USR_LIB}/bin/${APP_UPPER}"
+    SERVICE_COMMAND="env HOME=${SYNOPKG_PKGVAR} LD_LIBRARY_PATH=${SYNOPKG_PKGDEST}/lib ${APP} --nobrowser --data=${SYNOPKG_PKGVAR}"
+fi
+
+SVC_BACKGROUND=y
+
+fix_permissions ()
+{
+    if [ ${SYNOPKG_DSM_VERSION_MAJOR} -lt 7 ]; then
+        if [ -f "${BWRAP}" ]; then
+            chown root:root "${BWRAP}"
+            chmod u+s "${BWRAP}"
+        fi
+
+        set_unix_permissions "${SYNOPKG_PKGVAR}"
+    fi
+}
+
+service_postinst ()
+{
+    # Move config.xml to .config
+    mv ${SYNOPKG_PKGDEST}/app/config.xml ${SYNOPKG_PKGVAR}
+
+    fix_permissions
+}
+
+service_postupgrade ()
+{
+    # Do an update check on start to avoid possible
+    # downgrade when synocommunity package is updated
+    touch ${SYNOPKG_PKGVAR}/update_required
+
+    fix_permissions
+}
